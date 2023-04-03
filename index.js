50c46b411f56da24de52b84731fe29fff6e9a923
theisolatedmonk <96rahulseth@gmail.com>
2023-04-03 12:57:36 +0530

    usig variable

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const ratemsg = document.querySelector('.ratemsg');
 const btn1 = document.getElementById('btn1');
 const btn2 = document.getElementById('btn2');
 const btn3 = document.getElementById('btn3');
@@ -7,37 +6,44 @@ const btn5 = document.getElementById('btn5');
 const submitbtn = document.getElementById('submitBtn');
 const container1 = document.getElementById('container01');
 const container2 = document.getElementById('container02');
+const point = document.getElementById('point');
 
 let button = submitbtn;
 button.disabled = true;
+let ratting = 0
 
 
 btn1.addEventListener('click', function() {
-    ratemsg.innerHTML = ' You selected 1 out of 5'
+    point.innerHTML = 1;
+    ratting = 1;
     button.disabled = false;
 })
 btn2.addEventListener('click', function() {
-    ratemsg.innerHTML = ' You selected 2 out of 5'
+    point.innerHTML = 2;
+    ratting = 2;
     button.disabled = false;
 })
 btn3.addEventListener('click', function() {
-    ratemsg.innerHTML = ' You selected 3 out of 5'
+    point.innerHTML = 3;
+    ratting = 3;
     button.disabled = false;
 })
 btn4.addEventListener('click', function() {
-    ratemsg.innerHTML = ' You selected 4 out of 5'
+    point.innerHTML = 4;
+    ratting = 4;
     button.disabled = false;
 })
 btn5.addEventListener('click', function() {
-    ratemsg.innerHTML = ' You selected 5 out of 5'
+    point.innerHTML = 5;
+    ratting = 5;
     button.disabled = false;
 })
 
-
-
 submitbtn.addEventListener('click', function() {
-    container1.classList.add('hidden');
-    container1.classList.remove('flex');
-    container2.classList.add('flex');
-    container2.classList.remove('hidden');
+    if (ratting) {
+        container1.classList.add('hidden');
+        container1.classList.remove('flex');
+        container2.classList.add('flex');
+        container2.classList.remove('hidden');
+    }
 });
