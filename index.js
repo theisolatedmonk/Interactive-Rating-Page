a222795a9c76bfe667092cd339366f9b3b6db376
theisolatedmonk <96rahulseth@gmail.com>
2023-04-03 11:39:53 +0530

    simple working commit

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const btn3 = document.getElementById('btn3');
 const btn4 = document.getElementById('btn4');
 const btn5 = document.getElementById('btn5');
 const submitbtn = document.getElementById('submitBtn');
-const container1 = document.getElementById('.container01');
-const container2 = document.getElementById('.container02');
+const container1 = document.getElementById('container01');
+const container2 = document.getElementById('container02');
 
 
 
@@ -20,28 +20,18 @@ btn2.addEventListener('click', function() {
 btn3.addEventListener('click', function() {
     ratemsg.innerHTML = ' You selected 3 out of 5'
 })
-btn1.addEventListener('click', function() {
+btn4.addEventListener('click', function() {
     ratemsg.innerHTML = ' You selected 4 out of 5'
 })
-btn1.addEventListener('click', function() {
+btn5.addEventListener('click', function() {
     ratemsg.innerHTML = ' You selected 5 out of 5'
 })
 
-// submitbtn.addEventListener('click', function() {
-//     container1.style.visibility = "hidden";
-//     container2.style.visibility = "visible";
-
-// })
-
-// submitbtn.addEventListener('click', function() {
-//         container1.style.display = "none";
-//         container2.style.display = "flex";
-
-//     })
-// console.log('sm', submitbtn);
 
 
 submitbtn.addEventListener('click', function() {
     container1.classList.add('hidden');
+    container1.classList.remove('flex');
+    container2.classList.add('flex');
     container2.classList.remove('hidden');
 });
